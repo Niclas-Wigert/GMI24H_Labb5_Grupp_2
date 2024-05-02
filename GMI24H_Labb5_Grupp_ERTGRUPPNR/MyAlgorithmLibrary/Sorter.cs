e44de79996a8c1b8685206d6e981db68c3af9a92
Eva Hedly <evahedly@gmail.com>
2024-05-02 11:59:28 +0200

    heap sort klar, men gärna debugga och sedan kommentering av koden

--- GMI24H_Labb5_Grupp_ERTGRUPPNR/MyAlgorithmLibrary/Sorter.cs
+++ GMI24H_Labb5_Grupp_ERTGRUPPNR/MyAlgorithmLibrary/Sorter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
@@ -18,20 +19,20 @@ namespace GMI24H_Labb5_Grupp_ERTGRUPPNR.MyAlgorithmLibrary
         //Bubblesort(Data: values[])
         //// Repeat until the array is sorted.
         //Boolean: not_sorted = True
-            //While(not_sorted)
-            //// Assume we won't find a pair to swap.
-            //not_sorted = False
-            //// Search the array for adjacent items that are out of order.
-                //For i = 0 To<length of values> - 1
-                //// See if items i and i - 1 are out of order.
-                //If(values[i] < values[i - 1]) Then
-                //// Swap them.
-                //Data: temp = values[i]
-                //values[i] = values[i - 1]
-                //values[i - 1] = temp
-                //// The array isn't sorted after all.
-                //not_sorted = True
-            //End If
+        //While(not_sorted)
+        //// Assume we won't find a pair to swap.
+        //not_sorted = False
+        //// Search the array for adjacent items that are out of order.
+        //For i = 0 To<length of values> - 1
+        //// See if items i and i - 1 are out of order.
+        //If(values[i] < values[i - 1]) Then
+        //// Swap them.
+        //Data: temp = values[i]
+        //values[i] = values[i - 1]
+        //values[i - 1] = temp
+        //// The array isn't sorted after all.
+        //not_sorted = True
+        //End If
         //Next i
         //End While
         //End Bubblesort
@@ -41,64 +42,109 @@ namespace GMI24H_Labb5_Grupp_ERTGRUPPNR.MyAlgorithmLibrary
             while (not_sorted)
             {
                 not_sorted = false;
-                for(int i = 0; i < arr.Length-1; i++)
+                for (int i = 0; i < arr.Length - 1; i++)
                 {
-                    if (arr[i] > arr[i+1]) 
+                    if (arr[i] > arr[i + 1])
                     {
                         int temp = arr[i];
-                        arr[i] = arr[i+1];
-                        arr[i+1] = temp;
-                        not_sorted=true;
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        not_sorted = true;
                     }
                 }
             }
         }
 
+
+        // Reference for HeapSort https://www.geeksforgeeks.org/heap-sort/
         public void HeapSort(int[] arr)
         {
-            arr = MakeHeap(arr);
-            foreach (int i in arr)
+            int heapSize = arr.Length;
+
+            for (int i = heapSize/2-1; i >= 0; i--)
             {
-                Console.Write(i + ",");
+                MakeHeap(arr, heapSize, i);
             }
-            Console.WriteLine();
-            for (int i = arr.Length - 1; i >= 0; i--)
+
+            for (int i = heapSize-1; i > 0; i--)
             {
-                Console.WriteLine(arr[0] + "," + arr[i]);
                 int temp = arr[0];
                 arr[0] = arr[i];
                 arr[i] = temp;
-                MakeHeap(arr);
-                Console.WriteLine(arr[0]+","+arr[i]);
-                Console.WriteLine();
+                MakeHeap(arr, i, 0);
             }
             foreach (int i in arr)
             {
-                Console.Write(i + ",");
+                Console.Write(i + " ");
             }
+
+            //arr = MakeHeap(arr);
+            //foreach (int i in arr)
+            //{
+            //    Console.Write(i + ",");
+            //}
+            //Console.WriteLine();
+            //for (int i = arr.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(arr[0] + "," + arr[i]);
+            //    int temp = arr[0];
+            //    arr[0] = arr[i];
+            //    arr[i] = temp;
+            //    MakeHeap(arr);
+            //    Console.WriteLine(arr[0] + "," + arr[i]);
+            //    Console.WriteLine();
+            //}
+            //foreach (int i in arr)
+            //{
+            //    Console.Write(i + ",");
+            //}
         }
-        private int[] MakeHeap(int[] arr)
+        private void MakeHeap(int[] arr, int heapSize, int i)
         {
-            for (int i = 0; i < arr.Length - 1; i++)
+            int largestIndex = i;
+
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < heapSize && arr[left] > arr[largestIndex])
             {
-                int index = i;
-
-                while (index != 0)
-                {
-                    int parent = (index - 1) / 2;
-                    if (arr[index] <= arr[parent])
-                    {
-                        break;
-                    }
-
-                    int temp = arr[index];
-                    arr[index] = arr[parent];
-                    arr[parent] = temp;
-
-                    index = parent;
-                }
+                largestIndex = left;
             }
-            return arr;
+
+            if ( right < heapSize && arr[right] > arr[largestIndex])
+            {
+                largestIndex = right;
+            }
+            
+            if (largestIndex != i)
+            {
+                int temp = arr[i];
+                arr[i] = arr[largestIndex];
+                arr[largestIndex] = temp;
+
+                MakeHeap(arr, heapSize, largestIndex);
+            }
+
+            //for (int i = 0; i < arr.Length - 1; i++)
+            //{
+            //    int index = i;
+
+            //    while (index != 0)
+            //    {
+            //        int parent = (index - 1) / 2;
+            //        if (arr[index] <= arr[parent])
+            //        {
+            //            break;
+            //        }
+
+            //        int temp = arr[index];
+            //        arr[index] = arr[parent];
+            //        arr[parent] = temp;
+
+            //        index = parent;
+            //    }
+            //}
+            //return arr;
         }
 
         public void InsertionSort(int[] arr)
